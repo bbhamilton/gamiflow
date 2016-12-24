c635efbf293cab7b330e1d927453c089f2b7cbda
Bartosz Bilicki <bartekbilicki@gmail.com>
2016-12-24 01:12:18 +0100

    bootstrap 4

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-const express = require('express')
-const passport = require('passport')
-const app = express()
+const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bodyParser = require('body-parser');
 
-app.set('view engine', 'pug')
-app.set('views', './templates')
+// database
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/gamiflow');
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'to jest tytuł',
-    message: 'a to treść'
-  })
-})
+// routing
+const routes = require('./routes/index');
+const users = require('./routes/users');
 
-app.use(function (req, res, next) {
-  res.status(404).send('Sorry cant find that!')
-})
+// express
+const app = express();
 
-app.listen(7777, () => {
+// passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// PUG enging template config
+app.set('view engine', 'pug');
+app.set('views', './templates');
+
+// PUG cache disabled
+app.disable('view cache');
+
+app.use('/', routes);
+
+// 404
+// app.use(function (req, res, next) {
+//   res.status(404).send('Sorry cant find that!');
+// });
+
+// body parser basic config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// serve files from public/
+app.use(express.static(__dirname + '/public'));
+
+// set port
+app.set('port', (process.env.PORT || 7777));
+
+// launch server on 7777
+app.listen(app.get('port'), () => {
   console.log('Gamiflow is running...')
 })
